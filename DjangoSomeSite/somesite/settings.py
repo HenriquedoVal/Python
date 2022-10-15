b1b3daac89b95cfd48dd21e3c75bb42d6fcbe924
Henrique <henrique.val@hotmail.com>
2022-10-15 02:23:13 -0300

    customize admin page

--- DjangoSomeSite/somesite/settings.py
+++ DjangoSomeSite/somesite/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'somesite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['somesite/templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
